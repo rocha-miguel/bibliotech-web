e933dbff0b7d215639681a873ce3b0513eca9899
rocha-miguel <miguelrocha1818@gmail.com>
2026-04-03 15:09:52 -0300

    finalização do component home, footer e navbar, implemtação da rota cadastrar e component cadastrar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
+import { Cadastrar } from './components/cadastrar/cadastrar';
 
 export const routes: Routes = [
-    {path: '', component: Home}
+    {path: '', component: Home},
+    {path: 'cadastrar', component: Cadastrar}
 ];
