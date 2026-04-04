17bb0979c33956f11964f8a8718dcae88a5c25f5
rocha-miguel <miguelrocha1818@gmail.com>
2026-04-03 21:24:56 -0300

    finalização do component cadastrar, conexão com o endpoint POST da api

--- src/app/components/cadastrar/cadastrar.ts
+++ src/app/components/cadastrar/cadastrar.ts
@@ -1,9 +1,54 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastrar',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './cadastrar.html',
   styleUrl: './cadastrar.css',
 })
-export class Cadastrar {}
+export class Cadastrar {
+
+  http = inject(HttpClient);
+
+  formulario = new FormGroup({
+    nome: new FormControl(''),
+    editora: new FormControl(''),
+    autor: new FormControl(''),
+    quantidade: new FormControl(0),
+    quantidadePaginas: new FormControl(0),
+    anoEdicao: new FormControl(0),
+    tipoLivro: new FormControl(0),
+    generoLivro: new FormControl(0)
+  });
+
+  cadastrar() {
+
+    const form = this.formulario.value;
+
+    const data = {
+      nome: form.nome ?? '',
+      editora: form.editora ?? '',
+      autor: form.autor ?? '',
+      quantidade: Number(form.quantidade ?? 0),
+      quantidadePaginas: Number(form.quantidadePaginas ?? 0),
+      anoEdicao: Number(form.anoEdicao ?? 0),
+      tipoLivro: Number(form.tipoLivro ?? 0),
+      generoLivro: Number(form.generoLivro ?? 0)
+    };
+
+    this.http.post('http://localhost:8000/api/livro', data, { responseType: 'text' })
+      .subscribe((resposta) => {
+      alert(resposta);
+      this.formulario.reset();
+
+      });
+
+  }
+}
