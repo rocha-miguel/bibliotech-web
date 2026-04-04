f793720b05d04bbb1bfab9f136cc5d257025012c
rocha-miguel <miguelrocha1818@gmail.com>
2026-04-04 13:44:01 -0300

    finalização do component livros

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { Cadastrar } from './components/cadastrar/cadastrar';
+import { Livros } from './components/livros/livros';
 
 export const routes: Routes = [
     {path: '', component: Home},
-    {path: 'cadastrar', component: Cadastrar}
+    {path: 'cadastrar', component: Cadastrar},
+    {path: 'livros', component: Livros}
 ];
---
+++ src/app/components/livros/livros.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-livros',
+  imports: [],
+  templateUrl: './livros.html',
+  styleUrl: './livros.css',
+})
+export class Livros implements OnInit{
+
+  http = inject(HttpClient);
+
+  
+
+  livros = signal<any[]>([]);
+
+  ngOnInit(): void {
+    this.listar();
+  }
+
+  listar() {
+  this.http.get<any[]>('http://localhost:8000/api/Livro')
+    .subscribe({
+      next: (livros) => {
+        console.log('LIVROS RECEBIDOS:', livros);
+        console.log('QUANTIDADE:', livros.length);
+        this.livros.set(livros);
+        console.log('ARRAY DA TELA:', this.livros);
+      },
+      error: (erro) => {
+        console.error('ERRO:', erro);
+      }
+    });
+}
+
+}
